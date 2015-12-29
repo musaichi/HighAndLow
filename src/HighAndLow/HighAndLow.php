6436ac56e0fe8a9c5cd6cc80f92be1a768552c81
musaichi <musaichich@gmail.com>
2015-12-30 00:32:04 +0900

    Update HighAndLow.php

--- src/HighAndLow/HighAndLow.php
+++ src/HighAndLow/HighAndLow.php
@@ -1,3 +1,23 @@
 <?php
 
 namespace HighAndLow
+
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat;
+
+class HighAndLow extends PluginBase implements Listener{
+
+public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+ switch ($command->getName()){
+  case "high";
+  break;
+  case "low";
+  break;
+}
+}
+}
